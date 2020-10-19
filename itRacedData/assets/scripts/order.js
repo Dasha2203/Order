4f0d37518af170303a9c14f56609823979fd20c0
Darya Korbut <kitkatlolipop@mail.ru>
2020-10-19 20:14:14 +0300

    refactor: formatting code

--- itRacedData/assets/scripts/order.js
+++ itRacedData/assets/scripts/order.js
@@ -11,16 +11,11 @@ let buttonRemove = document.createElement('div');
 clearForm.addEventListener('click', () => formOrder.reset() );
 
 
-addPremises.addEventListener('click', () => { // обрабатываем нажатие на добваления нового айтема
-    
-    // зде копирую блок, чтобы потом такой же его вставить
+addPremises.addEventListener('click', () => { 
+
     let premis = document.querySelector('.premises').cloneNode(true);
-    console.log('скопированный элемент')
-    console.log(premis)
     premis.querySelector('.Address').value = '';
     premis.querySelector('.Unit').value = '';
-    // у первого айтема нет кнопки удалить, поэтому
-    // я здесь проверяю если всего их один, то первому добавляю кнопку удалить
     if (premises.length === 1) { 
         premises[0].append(buttonRemove)
         let buttonRemove1 = document.createElement('div');
@@ -30,52 +25,23 @@ addPremises.addEventListener('click', () => { // обрабатываем наж
         premis.append(buttonRemove1);
         removeItem(removePremises[0], 0)
     }
-    //здесь переобъявляю кнопку
-    
-    console.log(buttonRemove)
-    // добавляю кнопку в блок, который буду вставлять
-    //premis.append(buttonRemove1)
-    console.log('блок, котоырй добавляем')
-    console.log(premis)
-    containerPremises.appendChild(premis)// вставляю сам блок
-    removePremises = document.querySelectorAll('.remove')// обновляю количество кнопок для удаления
-    premises = document.querySelectorAll('.premises'); // обновляю количество блоков
-    console.log('remove: ' + removePremises.length)
-    console.log('all: ' + premises.length)
+    containerPremises.appendChild(premis)
+    removePremises = document.querySelectorAll('.remove')
+    premises = document.querySelectorAll('.premises');
     removeItem(removePremises[removePremises.length-1],removePremises.length-1);
-     console.log('все блоки')
-    console.log(premises);
+
 })
 
 
 
 function removeItem(removeButton,index) {
-
-        // if (removePremises.length === 2) {
-        //     removePremises[0].addEventListener('click', () => { 
-        //         containerPremises.removeChild(premises[0]);
-        //         removePremises = document.querySelectorAll('.remove')
-        //         premises = document.querySelectorAll('.premises');
-        //         console.log('то, что яхочу удалть');
-        //         console.log(removePremises[0])
-        //         if (premises.length === 1) {
-        //             premises[0].removeChild(removePremises[0])
-        //         }
-        //         console.log('после удаления ' + removePremises.length)
-        //     })
-        // }
-
         removeButton.addEventListener('click', () => { 
-            console.log('clock')
             containerPremises.removeChild(removeButton.parentNode);
             removePremises = document.querySelectorAll('.remove')
             premises = document.querySelectorAll('.premises');
-            console.log('то, что яхочу удалть');
-            console.log(removePremises[0])
             if (premises.length === 1) {
                 premises[0].removeChild(removePremises[0])
             }
-            console.log('после удаления ' + removePremises.length)
         })
 
 }
