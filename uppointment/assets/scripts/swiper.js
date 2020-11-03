fd8a52b780bb0db4d34e8cf803ada2a1b5185fa1
Dasha <dashkakorbut@gmail.com>
2020-11-03 17:38:15 +0300

    feat: add swiper

---
+++ uppointment/assets/scripts/swiper.js
@@ -0,0 +1,16 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
